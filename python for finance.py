a219a978cd36c3d17c63d61d02b7750542c10531
hellenjia <100323980+hellenjia@users.noreply.github.com>
2023-03-13 19:57:36 -0300

    Add files via upload

---
+++ python for finance.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+secret_num= 25
+
+
+# In[2]:
+
+
+secret_num += 1
+
+
+# In[3]:
+
+
+secret_num
+
+
+# In[4]:
+
+
+secret_num *= 2
+
+
+# In[5]:
+
+
+secret_num
+
+
+# In[6]:
+
+
+secret_num += 4
+
+
+# In[7]:
+
+
+secret_num
+
+
+# In[8]:
+
+
+secret_num /= 2
+
+
+# In[9]:
+
+
+secret_num
+
+
+# In[10]:
+
+
+secret_num -= 25
+
+
+# In[11]:
+
+
+secret_num
+
+
+# In[12]:
+
+
+print(secret_num)
+
+
+# In[13]:
+
+
+a = "55"
+
+
+# In[14]:
+
+
+type(a)
+
+
+# In[15]:
+
+
+b = 19.4
+
+
+# In[16]:
+
+
+type(b)
+
+
+# In[17]:
+
+
+c = ""
+
+
+# In[18]:
+
+
+type(c)
+
+
+# In[19]:
+
+
+movie = "Zoolander"
+
+
+# In[21]:
+
+
+first = "Nico"
+
+
+# In[22]:
+
+
+last = "Hulkenberg"
+
+
+# In[25]:
+
+
+full_name = first + " " + last
+
+
+# In[26]:
+
+
+full_name
+
+
+# In[27]:
+
+
+initials = first[0]+ last[0]
+
+
+# In[28]:
+
+
+print(initials)
+
+
+# In[29]:
+
+
+initials_2 = first[0]+ "."+last[0]+"."
+
+
+# In[30]:
+
+
+print(initials_2)
+
+
+# In[31]:
+
+
+nickname = last[:4]
+
+
+# In[32]:
+
+
+print(nickname)
+
+
+# In[33]:
+
+
+first_name= input("What is your name?")
+
+
+# In[35]:
+
+
+first_name
+
+
+# In[36]:
+
+
+age = input("How old are you?")
+
+
+# In[37]:
+
+
+age
+
+
+# In[ ]:
+
+
+first_name= input("What is your name?")
+print ("Hi there"+first_name+"!!")
+
+
+# In[ ]:
+
+
+first_name= input("What is your name?")
+print ("Hi there"+first_name+"!!")
+
+
+# In[ ]:
+
+
+
+
